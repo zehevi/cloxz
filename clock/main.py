d4f87852ea9d18e6a505ea2959d6ce00cda92259
Zehevi Apini <zeheviapini@gmail.com>
2024-09-18 15:16:35 +0300

    Add modify command

--- clock/main.py
+++ clock/main.py
@@ -13,6 +13,7 @@ CSV_FILE_PATH = DATA_DIR / CSV_FILE
 ACTIONS = {
     'in': 'Add a clock-in item',
     'out': 'Add a clock-out item',
+    'modify': 'Modify an existing entry',
     'list': 'List all items',
     'delete': 'Delete an item',
     'pwd': 'Prints the data directory path'
@@ -59,6 +60,8 @@ def main(ctx, action: str, text: str):
             print_csv_directory()
         case "in" | "out":
             add_clock_entry(filename, text, action)
+        case "modify":
+            modify_entry(filename)
         case "list":
             month = click.prompt(
                 'Enter the month (name, number)', type=validate_month, default="current")
@@ -153,6 +156,50 @@ def delete_entry(filename: str):
             click.echo(f"Deleted entry at line {line_number}")
         except:
             click.echo("Failed to delete entry")
+    else:
+        click.echo(f"Invalid line number: {line_number}")
+
+
+def modify_entry(filename: str):
+    """Modify an entry in the CSV file."""
+    entries = []
+
+    with open(filename, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        click.echo(click.style(
+            "Entries:", fg='blue', bold=True))
+        click.echo(click.style(
+            "Line | Date | Time | Action | Customer", fg='green', bold=True))
+        for i, row in enumerate(reader, start=1):
+            date, time, action, customer = row
+            click.echo(click.style(
+                f"{i:>5} | {date:>10} | {time:>8} | {action:>6} | {customer}", fg='white'))
+            entries.append(row)
+
+    line_number = click.prompt(
+        '\nEnter the line number of the entry you want to modify', type=int)
+
+    if 1 <= line_number <= len(entries):
+        click.echo(f"Current entry: {entries[line_number - 1]}")
+        new_date = click.prompt(
+            'Enter the new date (YYYY-MM-DD)', type=str, default=entries[line_number - 1][0])
+        new_time = click.prompt(
+            'Enter the new time (HH:MM:SS)', type=str, default=entries[line_number - 1][1])
+        new_action = click.prompt(
+            'Enter the new action', type=str, default=entries[line_number - 1][2])
+        new_customer = click.prompt(
+            'Enter the new customer', type=str, default=entries[line_number - 1][3])
+
+        entries[line_number - 1] = [new_date,
+                                    new_time, new_action, new_customer]
+
+        try:
+            with open(filename, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(entries)
+            click.echo(f"Modified entry at line {line_number}")
+        except:
+            click.echo("Failed to modify entry")
     else:
         click.echo(f"Invalid line number: {line_number}")
 
