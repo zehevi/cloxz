7617bf01547c65e4f8eb818dfcd5026c519828ca
Zehevi Apini <zeheviapini@gmail.com>
2024-12-11 15:01:50 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloxz",
-    version="0.4.7",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=["typer", "rich"],
     entry_points={
